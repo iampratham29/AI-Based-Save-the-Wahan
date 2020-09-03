239c4d676835174dc12c2117df6e1e1777206751
Vedangi Deshpande <vedangi.deshpande18@vit.edu>
2020-09-03 22:29:34 +0530

    Done Bifurcation.

--- Hand_detection.py
+++ Hand_detection.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 face_cascade=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade=cv2.CascadeClassifier('haarcascade_eye.xml')
@@ -7,19 +8,23 @@ cap=cv2.VideoCapture(0)
 while True:
     ret,image=cap.read()
     grey = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-    #faces=face_cascade.detectMultiScale(grey,1.2,5)
-    #eyes=eye_cascade.detectMultiScale(grey,1.2,5)
+
     hands=hand_cascade.detectMultiScale(grey,1.1,5)
 
     for (X,Y,W,H) in hands:
         print(X,Y,W,H)
         cv2.rectangle(image, (X, Y), (X + W, Y + H), (0, 255, 0), 2)
-        #cv2.rectangle(grey, (X, Y), (X + W, Y + H), (255, 0, 0), 2)
-        #roi_grey = grey[Y:Y + H, X:X + W]
-        roi_color = image[Y:Y + H, X:X + W]
 
-    cv2.imshow("image",image)
-    #cv2.imshow("grey",grey)
+        roi_color = image[Y:Y + H, X:X + W]
+    tmp=np.zeros((480,640))
+    tmp1=image[:,320:]
+    tmp2 = image[:, :320]
+    #cv2.imshow("image",image)
+
+    cv2.imshow("sample", tmp1)
+    cv2.imshow("sample2", tmp2)
+
+    print(image.shape)
     k=cv2.waitKey(1)
     if k==32:
         break
